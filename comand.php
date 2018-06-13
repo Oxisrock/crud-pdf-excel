3d0618f0990574b077529e08d1fb568412f89e30
oxisrock <franciscoaular18@gmail.com>
2018-06-13 11:47:28 -0400

    probando nuevos metodos de usar la terminal

--- comand.php
+++ comand.php
@@ -1,4 +1,11 @@
 <?php
-$salida = shell_exec('ls');
-echo "<pre>$salida</pre>";
+$salida = 'cd /var/www && ls ';
+while (@ ob_end_flush());
+$proc = popen($salida,'r');
+echo "<pre>";
+while (!feof($proc)) {
+  echo fread($proc, 4096);
+  @ flush();
+}
+echo "</pre>";
  ?>
