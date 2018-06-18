b6db91f62a8162dbb310b1913db9866871655daf
oxisrock <franciscoaular18@gmail.com>
2018-06-18 13:28:41 -0400

    subiendo todo a github

--- form.php
+++ form.php
@@ -7,11 +7,24 @@ use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 /*Toma el 'Last Name' de index.html*/
 $fname = $_POST['fname'];
-$lname = $_POST['lname'];
-$dni = $_POST['dni'];
-$age = $_POST['age'];
-$job = $_POST['job'];
-$cel = $_POST['cel'];
+$address = $_POST['address'];
+$city = $_POST['city'];
+$state = $_POST['state'];
+$zip = $_POST['zip'];
+$tel = $_POST['tel'];
+$lic = $_POST['lic'];
+$iss = $_POST['iss'];
+$md = $_POST['md'];
+$do = $_POST['do'];
+$pa = $_POST['pa'];
+$cr = $_POST['cr'];
+$an = $_POST['an'];
+$op = $_POST['op'];
+$nregister = $_POST['nregister'];
+// $dni = $_POST['dni'];
+// $age = $_POST['age'];
+// $job = $_POST['job'];
+// $cel = $_POST['cel'];
 
 /*Crea nueva instancia para leer un documento XLSX*/
 $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
@@ -22,12 +35,20 @@ $spreadsheet = $reader->load("tmp.xlsx");
 $sheet = $spreadsheet->getActiveSheet();
 /*Guarda la variable que recibimos de index.html en la celda requerida.*/
 $sheet->setCellValue('A2', $fname);
-$sheet->setCellValue('B2', $lname);
-$sheet->setCellValue('C2', $dni);
-$sheet->setCellValue('D2', $age);
-$sheet->setCellValue('E2', $job);
-$sheet->setCellValue('F2', $cel);
-
+$sheet->setCellValue('B2', $address);
+$sheet->setCellValue('C2', $city);
+$sheet->setCellValue('D2', $zip);
+$sheet->setCellValue('E2', $zip);
+$sheet->setCellValue('F2', $tel);
+$sheet->setCellValue('G2', $lic);
+$sheet->setCellValue('H2', $iss);
+$sheet->setCellValue('I2', $md);
+$sheet->setCellValue('J2', $do);
+$sheet->setCellValue('K2', $pa);
+$sheet->setCellValue('L2', $cr);
+$sheet->setCellValue('M2', $an);
+$sheet->setCellValue('N2', $op);
+$sheet->setCellValue('O2', $nregister);
 /*Se carga una nueva instancia para escribir, usando nuestro documento XLSX*/
 $writer = new \PhpOffice\PhpSpreadsheet\Writer\Xlsx($spreadsheet);
 
@@ -64,5 +85,5 @@ $sheet->fromArray(
         'A4'         // Top left coordinate of the worksheet range where
                      //    we want to set these values (default is A1)
     );
-
+    
 ?>
